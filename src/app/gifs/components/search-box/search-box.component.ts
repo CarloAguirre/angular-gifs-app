7b10acf60af92da40ce66e9fcffad6edb2c62796
Carlo Aguirre <carlo_aguirre@outlook.cl>
2024-05-01 14:35:21 -0400

    search placeholder changed

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -7,7 +7,7 @@ import { GifsService } from '../../services/gifs.service';
     <h5>Buscar:</h5>
     <input type="text"
     class="form-control"
-    placeholder="Buscar gif..."
+    placeholder="Buscar concordancias..."
     #txtTagInput
     (keyup.enter)="searchTag()"
    >
